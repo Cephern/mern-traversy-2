e36f0fb83ca58db0116d986bfa61ac74b34fe0b0
JagMax <gewoonmaks@gmail.com>
2021-04-07 17:32:43 +0300

    finale project

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,50 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./comps/layout/Navbar";
 import Home from "./comps/pages/Home";
 import About from "./comps/pages/About";
+import Register from "./comps/auth/Register";
+import Login from "./comps/auth/Login";
+import Alerts from "./comps/layout/Alerts";
+
+import PrivateRoute from "./comps/routing/PrivateRout";
 
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
+
+import setAuthToken from "./utils/setAuthToken";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route exact path="/about">
-                <About />
-              </Route>
-            </Switch>
-          </div>
-        </>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <PrivateRoute exact path="/" component={Home} />
+                  <Route exact path="/about">
+                    <About />
+                  </Route>
+                  <Route exact path="/register">
+                    <Register />
+                  </Route>
+                  <Route exact path="/login">
+                    <Login />
+                  </Route>
+                </Switch>
+              </div>
+            </>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
 }
 
